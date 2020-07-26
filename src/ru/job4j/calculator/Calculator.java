ebde145350f7699ed4de753cabee59b511c22007
AlexanderBanar <s595659@mail.ru>
2020-07-26 17:53:03 +0300

    4.1. Аргументы [#333389]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -2,7 +2,7 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         int one = 1;
         int two = 2;
         int onePlusTwo = one + two;
@@ -20,4 +20,6 @@ public class Calculator {
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
     }
+
+
 }
