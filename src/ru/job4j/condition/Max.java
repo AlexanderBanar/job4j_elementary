c48d1fdc6c3361866f6150ca568bedfb68a2e8e8
AlexanderBanar <s595659@mail.ru>
2020-08-01 09:09:17 +0300

    4.3.1. А существует ли треугольник? [#333346]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -8,7 +8,7 @@ public class Max {
     }
 
     public static void main(String[] args) {
-        int result = Max.max(2,3);
+        int result = Max.max(2, 3);
         System.out.println(result);
 
         result = Max.max(4, 1);
---
+++ src/ru/job4j/condition/Triangle.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exist(double ab, double ac, double bc) {
+        boolean exist = (ab + ac > bc) && (ac + bc > ab) && (ab + bc > ac);
+        return exist;
+    }
+
+    public static void main(String[] args) {
+        boolean exist = Triangle.exist(1.0, 2.0, 3.0);
+        System.out.println(exist);
+
+        exist = Triangle.exist(7.0, 15.0, 9.0);
+        System.out.println(exist);
+    }
+}
