cf185058c2ea2c9318e9330e58989531096b7ccb
AlexanderBanar <s595659@mail.ru>
2020-08-05 07:37:22 +0300

    5.8. Именование классов и переменных [#333443]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -3,9 +3,7 @@ package ru.job4j.converter;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class ConverterTest {
-
     @Test
     public void whenConvert140RblThen2Euro() {
         int in = 140;
