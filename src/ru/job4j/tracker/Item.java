ca2a83158cd4185b6f7e7d2ba357b4b85cd2234a
AlexanderBanar <s595659@mail.ru>
2020-08-23 09:31:31 +0300

    2. Перегрузка конструктора [#333548]

---
+++ src/ru/job4j/tracker/Item.java
@@ -0,0 +1,15 @@
+package ru.job4j.tracker;
+
+public class Item {
+    private int id;
+    private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+    }
+
+    public Item(String name) {
+    }
+}
