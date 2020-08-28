44ec760b729a5a776dc3386c294c0ac499f3beb4
AlexanderBanar <s595659@mail.ru>
2020-08-28 17:47:47 +0300

    6. Метод замены заявки. Tracker.replace [#333560]

--- src/ru/job4j/tracker/Tracker.java
+++ src/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,8 @@ public class Tracker {
     }
 
     public Item findById(int id) {
-        Item rsl = null;
-        for (int index = 0; index < size; index++) {
-            Item item = items[index];
-            if (item.getId() == id) {
-                rsl = item;
-                break;
-            }
-        }
-        return rsl;
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     public Item[] findAll() {
@@ -48,5 +41,24 @@ public class Tracker {
             }
         }
         return Arrays.copyOf(resultWithNull, counter);
+    }
+
+    private int indexOf(int id) {
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public boolean replace(int id, Item item) {
+        int rsl = indexOf(id);
+        if (rsl != -1) {
+            items[rsl].setName(item.getName());
+        }
+        return rsl != -1 ? true : false;
     }
 }
