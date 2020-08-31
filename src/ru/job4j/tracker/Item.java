22c1ce101deeb39d9f95c6cc82862e5cf427d5e9
AlexanderBanar <s595659@mail.ru>
2020-08-31 20:58:30 +0300

    7. Date. Отображение даты. [#333529]

--- src/ru/job4j/tracker/Item.java
+++ src/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    LocalDateTime created = LocalDateTime.now();
 
     @Override
     public String toString() {
@@ -26,5 +30,10 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
--- src/ru/job4j/tracker/StartUI.java
+++ src/ru/job4j/tracker/StartUI.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
@@ -7,5 +10,9 @@ public class StartUI {
         Tracker tracker = new Tracker();
         tracker.add(item);
         System.out.println(tracker.findById(1));
+        Item forDate = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = forDate.getCreated().format(formatter);
+        System.out.println(currentDateTimeFormat);
     }
 }
