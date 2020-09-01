fc2d3a310e4104304bfced16a10d8b329c20fd06
AlexanderBanar <s595659@mail.ru>
2020-09-01 18:24:18 +0300

    3. Интерфейс Input [#333486]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,31 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public String drive() {
+        String driving = "On parking"
+        if (passengers(4) == 0) {
+            driving = "On drive ...";
+        }
+        return driving;
+    }
+
+    @Override
+    public int passengers(int quantityOfPassengers) {
+        int forDrive = -1;
+        if (quantityOfPassengers > 0) {
+            forDrive = 0;
+        }
+        return forDrive;
+    }
+
+    @Override
+    public double fuel(double volumeOfFuel) {
+        double price = 0.00;
+        double cost = 0.00;
+        if (drive().equals("On driving ...")) {
+            cost = price * volumeOfFuel;
+        }
+        return cost;
+    }
+}
