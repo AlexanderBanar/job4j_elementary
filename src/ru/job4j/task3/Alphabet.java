daad94570bfd71aa99da5d800e8f8fe02c658d7b
Alexander.Banar <s6M-HN2-EpL-9CX>
2020-11-21 10:15:56 +0300

    Упражнения 21.11 / 1015

---
+++ src/ru/job4j/task3/Alphabet.java
@@ -0,0 +1,17 @@
+package ru.job4j.task3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Alphabet {
+    public static String reformat(String s) {
+        List<String> temp = Arrays.asList(s.split(""));
+
+
+
+
+        return s;
+    }
+}
